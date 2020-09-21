911f05a171dbc50ad8ecb93554f41be266fde7e2
DenisIsmagilov1 <denchikism789@gmail.com>
2020-09-22 02:59:30 +0300

    Fix html, css

--- src/app/employees/pages/create/employees.create.component.ts
+++ src/app/employees/pages/create/employees.create.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { GetDepartments } from 'src/app/reducers/departmens/departments.actions';
 import { Department, DepartmentsState } from 'src/app/reducers/departmens/departments.reducer';
 import { selectDepartments } from 'src/app/reducers/departmens/departments.selectors';
 import * as employeeActions from 'src/app/reducers/employees/employees.actions';
@@ -11,12 +12,16 @@ import * as employeeActions from 'src/app/reducers/employees/employees.actions';
   templateUrl: './employees.create.component.html',
   styleUrls: ['./employees.create.component.scss']
 })
-export class EmployeesCreateComponent {
+export class EmployeesCreateComponent implements OnInit {
   public firstName: string;
   public lastName: string;
   public departmentId: number;
   public departments: Observable<Department[]> = this.store$.pipe(select(selectDepartments));
   constructor(private store$: Store<DepartmentsState>, private router: Router) { }
+
+  ngOnInit() {
+    this.store$.dispatch(new GetDepartments());
+  }
 
   createEmployee() {
     this.store$.dispatch(new employeeActions.CreateEmployee({
